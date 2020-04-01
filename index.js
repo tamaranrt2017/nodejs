8bb1f826e13da168928299e1f749a8bda36ca35c
tamaranrt2017 <62066222+tamaranrt2017@users.noreply.github.com>
2020-04-01 18:16:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const mathjs = require("mathjs");
+
+const izracunaj = (a, b) => {
+  console.log("Sabiranje: ", mathjs.add(a, b));
+  console.log("Najmanji broj", mathjs.min(a, b));
+  console.log(`Logaritam od ${a}: `, mathjs.log(a));
+  console.log(`Koren od ${a}: `, mathjs.sqrt(a));
+  console.log("Stepen: ", mathjs.pow(a, b));
+};
+
+izracunaj(4, 2);
