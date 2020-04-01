b2b8529ea8d95f1dbd7b085d8da8caca5a4c7979
tamaranrt2017 <62066222+tamaranrt2017@users.noreply.github.com>
2020-04-01 17:56:01 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const izracunaj = (a, b) => {
-  console.log("Sabiranje: ", mathjs.add(a, b));
-  console.log("Najmanji broj", mathjs.min(a, b));
-  console.log(`Logaritam od ${a}: `, mathjs.log(a));
-  console.log(`Koren od ${a}: `, mathjs.sqrt(a));
-  console.log("Stepen: ", mathjs.pow(a, b));
-};
-
-izracunaj(4, 2);
